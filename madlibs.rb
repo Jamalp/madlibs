56f621af234a07de0a1731a9483b727015b5390a
Jamal Powell <jamalkpowell@gmail.com>
2013-06-18 12:03:49 -0400

    First Commit

---
+++ madlibs.rb
@@ -0,0 +1,22 @@
+puts "Give me an Animal"
+animal = gets.chomp
+
+puts "And its name?"
+name = gets.chomp
+
+puts "Give me another Animal"
+animal2 = gets.chomp
+
+puts "whats his name?"
+name2 = gets.chomp
+
+puts "Give me a place"
+place = gets.chomp
+
+puts "give me a verb"
+verb = gets.chomp
+
+# puts "and give me a noun"
+# noun = gets.chomp
+
+puts "#{name} the #{animal} & #{name2} the #{animal2} went to the #{place} to #{verb} the night away."
